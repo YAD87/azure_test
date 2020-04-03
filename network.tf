c22e74ee06f7b50de0d5c743ba2dc4c82938ce69
Denis <people-time@yandex.ru>
2020-04-03 22:35:40 +0300

    Update network.tf
    rg

--- network.tf
+++ network.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "rppg_vnet" {
   name 					= "Rppg-VNet"
   address_space 		= [var.vnet_cidr]
   location 				= var.location
-  resource_group_name 	= "${azurerm_resource_group.rppg_rg.name}"
+  resource_group_name 	= azurerm_resource_group.rppg_rg.name
   
   tags = {
 	environment = "dev"
