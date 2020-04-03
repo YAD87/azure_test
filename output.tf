efe9d6c2f03ab78f0adf387573c228f58d9909cc
Denis <people-time@yandex.ru>
2020-04-03 22:47:42 +0300

    Update output.tf
    back

--- output.tf
+++ output.tf
@@ -2,9 +2,6 @@ output "web" {
   value = "${azurerm_public_ip.web_pip.ip_address}"
 }
 
-output "backend" {
-  value = "${private_ip_address.private_nic_backend.ip_address}"
-}
 output "postgresql_server_name" {
   value = "${azurerm_postgresql_server.postgresql_server.name}"
 }
