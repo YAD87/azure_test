eeab917bbc460e5b6edb516172b1b35f974f14ef
Denis <people-time@yandex.ru>
2020-04-03 23:11:36 +0300

    Update vm.tf
    vm

--- vm.tf
+++ vm.tf
@@ -1,7 +1,7 @@
 resource "azurerm_linux_virtual_machine" "web" {
   name                  = "Rppg-WebVM"
   location              = var.location
-  resource_group_name   = "${azurerm_resource_group.rppg_rg.name}"
+  resource_group_name   = azurerm_resource_group.rppg_rg.name
   network_interface_ids = [azurerm_network_interface.public_nic_web.id]
   vm_size               = "Standard_DS1_v2"
 
@@ -18,19 +18,14 @@ resource "azurerm_linux_virtual_machine" "web" {
    os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
-        create_option     = "FromImage"
         storage_account_type = "Standard_LRS"
     }
 
-  os_profile {
+ 
     computer_name  = "webserver"
     admin_username = var.vm_username
-  }
+    disable_password_authentication = true
 
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
 
   admin_ssh_key {
         username       = "azureuser"
@@ -63,19 +58,13 @@ resource "azurerm_linux_virtual_machine" "backend" {
    os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
-        create_option     = "FromImage"
         storage_account_type = "Standard_LRS"
     }
 
-  os_profile {
+  
     computer_name  = "backend"
     admin_username = var.vm_username
-  }
-
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+    disable_password_authentication = true
 
   admin_ssh_key {
         username       = "azureuser"
