6b047760b71780a8e4c3c0b4e598983350ea5ad5
Denis <people-time@yandex.ru>
2020-04-03 23:04:56 +0300

    Update vm.tf
    vv

--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_machine" "web" {
+resource "azurerm_linux_virtual_machine" "web" {
   name                  = "Rppg-WebVM"
   location              = var.location
   resource_group_name   = "${azurerm_resource_group.rppg_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic_web.id}"]
+  network_interface_ids = [azurerm_network_interface.public_nic_web.id]
   vm_size               = "Standard_DS1_v2"
 
 #This will delete the OS disk and data disk automatically when deleting the VM
@@ -46,8 +46,8 @@ resource "azurerm_virtual_machine" "web" {
 resource "azurerm_virtual_machine" "backend" {
   name                  = "Rppg-BackendVM"
   location              = var.location
-  resource_group_name   = "${azurerm_resource_group.rppg_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.private_nic_backend.id}"]
+  resource_group_name   = azurerm_resource_group.rppg_rg.name
+  network_interface_ids = [azurerm_network_interface.private_nic_backend.id]
   vm_size               = "Standard_DS1_v2"
 
 #This will delete the OS disk and data disk automatically when deleting the VM
