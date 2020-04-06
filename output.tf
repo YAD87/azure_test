2984d1974251346a2fc4121cebe6d5681cc3af48
Denis <people-time@yandex.ru>
2020-04-06 19:20:10 +0300

    Update output.tf
    nics

--- output.tf
+++ output.tf
@@ -2,6 +2,22 @@ output "web" {
   value = "${azurerm_public_ip.web_pip.ip_address}"
 }
 
+output "managment" {
+  value = "${azurerm_public_ip.managment_pip.ip_address}"
+}
+
+output "backend" {
+  value = "${azurerm_network_interface.private_nic_backend.private_ip_address}"
+}
+
+output "db" {
+  value = "${azurerm_network_interface.private_nic_db.private_ip_address}"
+}
+
+
+
+
+
 
 
 
