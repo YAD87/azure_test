8be756892d204d667d68486ff9e0a0c3ff1d0434
Denis <people-time@yandex.ru>
2020-04-06 19:26:14 +0300

    Update vm.tf
    managment os

--- vm.tf
+++ vm.tf
@@ -1,3 +1,45 @@
+resource "azurerm_linux_virtual_machine" "managment" {
+  name                  = "Rppg-ManagmentVM"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rppg_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic_managment.id]
+  size               = "Basic_A1"
+
+#This will delete the OS disk and data disk automatically when deleting the VM
+  
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+   os_disk {
+        name              = "web-os"
+        caching           = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+ 
+    computer_name  = "ops"
+    admin_username = var.vm_username
+    disable_password_authentication = true
+
+
+  admin_ssh_key {
+        username       = "azureuser"
+        public_key     =  file("/home/denys/.ssh/rppg.pub")
+    }
+
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+
+
 resource "azurerm_linux_virtual_machine" "web" {
   name                  = "Rppg-WebVM"
   location              = var.location
