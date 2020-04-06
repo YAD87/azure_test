87053b7539ee4595c8643349aa5c1c31f580d0da
Denis <people-time@yandex.ru>
2020-04-06 15:34:00 +0300

    Update vm.tf
    add db vm

--- vm.tf
+++ vm.tf
@@ -76,3 +76,44 @@ resource "azurerm_linux_virtual_machine" "backend" {
     environment = "dev"
   }
 }
+
+
+resource "azurerm_linux_virtual_machine" "db" {
+  name                  = "Rppg-DBVM"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rppg_rg.name
+  network_interface_ids = [azurerm_network_interface.private_nic_db.id]
+  size               = "Basic_A1"
+
+#This will delete the OS disk and data disk automatically when deleting the VM
+  
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+   os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+  
+    computer_name  = "db"
+    admin_username = var.vm_username
+    disable_password_authentication = true
+
+  admin_ssh_key {
+        username       = "azureuser"
+        public_key     =  file("/home/denys/.ssh/rppg.pub")
+    }
+
+
+  tags = {
+    environment = "dev"
+  }
+}
+
