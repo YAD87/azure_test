63dde8645975f00162f82ef4d6725bc164722758
Denis <people-time@yandex.ru>
2020-04-06 15:47:33 +0300

    Update postgresql.tf
    comment postgres

--- postgresql.tf
+++ postgresql.tf
@@ -1,32 +1,32 @@
-resource "azurerm_postgresql_server" "postgresql_server" {
-  name                = var.postgresql_server_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rppg_rg.name
+#resource "azurerm_postgresql_server" "postgresql_server" {
+ # name                = var.postgresql_server_name
+  #location            = var.location
+  #resource_group_name = azurerm_resource_group.rppg_rg.name
 
-  sku_name     = "B_Gen5_2"
+  #sku_name     = "B_Gen5_2"
    
 
-  storage_profile {
-    storage_mb            = "5120"
-    backup_retention_days = "7"
-    geo_redundant_backup  = "Disabled"
-    auto_grow             = "Enabled"
-  }
+  #storage_profile {
+   # storage_mb            = "5120"
+    #backup_retention_days = "7"
+    #geo_redundant_backup  = "Disabled"
+    #auto_grow             = "Enabled"
+  #}
 
-  administrator_login          = var.postgresql_server_administrator_login
-  administrator_login_password = var.postgresql_server_administrator_login_password
-  version                      = var.postgresql_server_version
-  ssl_enforcement              = var.postgresql_server_ssl_enforcement
+  #administrator_login          = var.postgresql_server_administrator_login
+  #administrator_login_password = var.postgresql_server_administrator_login_password
+  #version                      = var.postgresql_server_version
+  #ssl_enforcement              = var.postgresql_server_ssl_enforcement
 
-  tags = {
-      environment = "dev"
-  }
-}
+  #tags = {
+   #   environment = "dev"
+  #}
+#}
 
-resource "azurerm_postgresql_database" "postgresql_database" {
-  name                = var.postgresql_database_name
-  resource_group_name = azurerm_resource_group.rppg_rg.name
-  server_name         = azurerm_postgresql_server.postgresql_server.name
-  charset             = var.postgresql_database_charset
-  collation           = var.postgresql_database_collation
-}
+#resource "azurerm_postgresql_database" "postgresql_database" {
+#  name                = var.postgresql_database_name
+#  resource_group_name = azurerm_resource_group.rppg_rg.name
+ # server_name         = azurerm_postgresql_server.postgresql_server.name
+ # charset             = var.postgresql_database_charset
+ # collation           = var.postgresql_database_collation
+#}
