3e65bcac6b3b156430210d309dd2a2e9fdaeb9ad
Denis <people-time@yandex.ru>
2020-04-21 13:47:13 +0300

    Update vm.tf
    check

--- vm.tf
+++ vm.tf
@@ -132,7 +132,7 @@ resource "azurerm_linux_virtual_machine" "backend-vp" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS "
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
